cb76043e821111ab5bd5612490927b8933218e71
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-03-29 23:41:56 -0300

    Preparing for Day6 lesson challange

--- src/modules/cards.ts
+++ src/modules/cards.ts
@@ -1,17 +1,116 @@
-import { readonly } from "vue";
+import { reactive, readonly } from "vue";
 
-interface State {
-  list: [];
+// ---------------------- Interface ------------------
+interface Card {
+  id: number;
+  name: string;
+  price: number;
+  attack: number;
+  defence?: number; // talvez nao venha nada nesse
+  // defence: number | undefined; // old syntax
 }
 
+interface State {
+  list: Card[];
+  busy: boolean;
+}
+// -------------------------------------------
+const state: State = reactive({
+  list: [],
+  busy: false
+});
 
-// the cards will go here!
-const state: State = {
-  list: []
+// ----------------Actions--------------------
+const actions = {
+  loadCards() {
+    mutations.setBusy(true);
+
+    const mockData = [
+      {
+        id: 1,
+        name: "Pikachu",
+        price: 10,
+        attack: 5,
+        defence: 10
+      },
+      {
+        id: 2,
+        name: "Bulbassaur",
+        price: 20,
+        attack: 7,
+        defence: 34
+      },
+      {
+        id: 3,
+        name: "Charmander",
+        price: 40,
+        attack: 6,
+        defence: 51
+      },
+      {
+        id: 4,
+        name: "Piplup",
+        price: 45,
+        attack: 11,
+        defence: 98
+      },
+      {
+        id: 5,
+        name: "Biduf",
+        price: 32,
+        attack: 87,
+        defence: 22
+      }
+    ];
+
+    console.log("Vamos carregar os cards");
+
+    setTimeout(() => {
+      // API CALLS GOES HERE
+
+      console.log("depois de 0.5s");
+      mockData.forEach(card => {
+        // console.log(card);
+        mutations.processCard(card);
+      });
+
+      mutations.setBusy(false);
+    }, 500);
+  }
 };
+//-----------------Mutations--------------------------//
+const mutations = {
+  setBusy(newValue: boolean) {
+    state.busy = newValue;
+  },
+
+  processCard(card: any) {
+    // verifying if it exists
+    const idx = state.list.findIndex(x => x.id === card.id);
+    console.log("process", card);
+
+    const newCard: Card = {
+      id: card.id,
+      name: card.name,
+      price: card.price,
+      attack: card.attack,
+      defence: card.defence
+    };
+
+    if (idx === -1) {
+      // if it doesnt exist, create a new one
+      state.list.push(newCard);
+    } else {
+      // editando o atual, pra evitar duplicidade
+      state.list[idx] = newCard;
+    }
+  }
+};
+//------------------------------------------------------//
 
 export default function useCards() {
   return readonly({
-    state
+    state,
+    actions
   });
 }
--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,29 +1,37 @@
 <template>
-  <div>
-    <h1>Lesson 2: Yoyo's</h1>
+  <div v-if="!busy">
+    {{ busy }}
+    <h1>{{ listOfCards.length }} Pokemons</h1>
+
+    <div v-if="listOfCards.length" class="cardContainer">
+      <!-- {{ listOfCards }} -->
+      <div v-for="card in listOfCards" :key="card.id" class="card">
+        <p>ID: {{ card.id }}</p>
+        {{ card.name }}
+      </div>
+    </div>
+
+    <h1 style="margin-top: 100px">Lesson 2: Yoyo's</h1>
     <options />
     <composition-api />
 
-    <h1>Lesson 2: Challenge</h1>
+    <!-- <h1>Lesson 2: Challenge</h1>
     <meu-composition />
-    <meu-option />
+    <meu-option /> -->
 
     <h1>Lesson 3: Yoyo's</h1>
     <div>Hello</div>
     <div>Total: {{ total }}</div>
 
     <options c="Componente options 1" :total="total" @inc="incHandler" />
-    <options c="Componente options 2" @inc="incHandler" />
-    <options c="Componente options 3" @inc="incHandler" />
-
     <composition-api text="Componente Composition 1" @inc="incHandler" />
-    <composition-api text="Componente Composition 2" @inc="incHandler" />
-    <composition-api text="Componente Composition 3" @inc="incHandler" />
   </div>
+
+  <div v-else>Loading your app...</div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from "vue";
+import { ref, defineComponent, computed } from "vue";
 import Options from "@/components/OptionsApi.vue";
 import CompositionApi from "@/components/CompositionApi.vue";
 
@@ -42,19 +50,51 @@ export default defineComponent({
   },
 
   setup() {
-    const auth = useAuth();
+    // const auth = useAuth();
     const cards = useCards();
-    
     const total = ref(0);
 
-    console.log("auth dentro de Componentes.vue", auth);
+    console.log("Components.cards", cards);
 
+    // Computed Functions
+    const listOfCards = computed(() => {
+      return cards.state.list;
+    }); // this form doesnt require 'return'
+    const busy = computed(() => cards.state.busy);
+    //functions
     const incHandler = () => {
       console.log("total", total.value);
       total.value += 1;
     };
 
-    return { total, incHandler };
+    cards.actions.loadCards();
+
+    return { total, incHandler, listOfCards, busy };
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.card {
+  border: 2px solid black;
+  padding: 1rem;
+  border-radius: 25px;
+  width: 5rem;
+  margin: 1rem;
+}
+
+.card p {
+  background-color: salmon;
+  border-radius: 25px;
+}
+
+.cardContainer {
+  border: 2px solid blue;
+  border-radius: 25px;
+  padding: 1rem;
+  margin: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+</style>
