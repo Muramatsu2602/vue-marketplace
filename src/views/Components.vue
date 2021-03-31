1e991728405be0cab1a2c2dee74167188d765d29
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-03-30 23:47:44 -0300

    debbuging

--- src/views/Components.vue
+++ src/views/Components.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="!busy">
     {{ busy }}
-    <h1>{{ listOfCards.length }} Pokemons</h1>
+    <h1>{{ listOfCards.length }} Cientistas</h1>
 
     <div v-if="listOfCards.length" class="cardContainer">
       <!-- {{ listOfCards }} -->
@@ -30,6 +30,7 @@
 import { ref, defineComponent, computed } from "vue";
 import Options from "@/components/OptionsApi.vue";
 import CompositionApi from "@/components/CompositionApi.vue";
+import "@/styles/global.css";
 
 import useAuth from "@/modules/auth";
 import useCards from "@/modules/cards";
@@ -67,11 +68,28 @@ export default defineComponent({
 
 <style lang='scss' scoped>
 .card {
-  border: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   padding: 1rem;
-  border-radius: 25px;
-  width: 5rem;
+  width: 350px;
+  /* height: 450px; */
+  height: 300px;
   margin: 1rem;
+  border-radius: 8px;
+
+  background-color: var(--color-white-background);
+  color: var(--color-text-title);
+  border: 3px solid var(--color-primary-dark);
+  font-weight: 500;
+
+  transition: border 0.2s !important;
+
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+    0 100px 80px rgba(0, 0, 0, 0.12);
 }
 
 .card p {
