32d3348ccf1650e5bdb0a550476c5e2e2beb0170
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-04-09 19:13:25 -0300

    box shadow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="header">
+      <h1>Home Page!</h1>
+    </div>
+    <div class="main">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
   </div>
 </template>
 
@@ -24,6 +29,24 @@ export default defineComponent({
 
   align-items: center;
   justify-content: space-between;
+}
+
+.main {
+  border: 5px solid var(--color-primary-light);
+  border-radius: 25px;
+  margin: 2rem;
+  padding: 2rem;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: space-between;
+
+  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+    0 100px 80px rgba(0, 0, 0, 0.12);
 }
 
 .container img {
